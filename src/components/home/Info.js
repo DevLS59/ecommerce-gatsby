d482e1410644712111d33c3cf8bffc112f7bcf69
devls <devls@yahoo.com>
2020-01-03 18:14:47 +0100

    background-image section info

---
+++ src/components/home/Info.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import {Link} from 'gatsby'
+
+
+import Title from '../globals/Title'
+export default function Info() {
+    return (
+       <section className="py-5">
+           <div className="container">
+               <Title title="Our Story" /> 
+               <div className="row">
+                   <div className="col-10 col-sm-8 mx-auto text-center">
+                       <p className="lead text-muted mb-5">
+                           Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident ea nisi adipisci, suscipit modi odit, dolorem soluta culpa odio distinctio blanditiis sapiente neque ut aperiam magnam laborum? Dolor velit odit molestias ipsa officia delectus ducimus labore nobis eligendi. Dolorum ad deserunt excepturi, velit impedit eaque. Cum inventore, veniam provident ab distinctio alias blanditiis ipsum fugiat ipsam corporis cumque odit, expedita animi totam dolorem, reprehenderit tenetur doloremque a natus est. Rem temporibus, ea illum nulla, aperiam voluptates esse sed, consequatur id voluptas delectus eos vero repudiandae ad quos quidem iusto dolorum quo veritatis. Sit blanditiis iste dicta illo aut. Reprehenderit, quos?
+                        </p>
+                        <Link to='/about'>
+                            <button className="btn btn-orange text-uppercase">About Pa  </button>
+                        </Link>
+                    </div>
+                </div>
+            </div>
+
+       </section>
+    )
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,18 +1,35 @@
 import React from "react"
-
+import { Link, graphql} from 'gatsby'
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import {FaGulp} from 'react-icons/fa'
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-   <FaGulp /> 
-    
-    
-  </Layout>
-)
 
+import BackgroundSection from '../components/globals/BackgroundSection'
+import Info from '../components/home/Info'
+
+
+const IndexPage = ( { data }) => (
+    <Layout>
+        <SEO title="Home" />
+        <BackgroundSection 
+        img={data.img.childImageSharp.fluid}
+        title="regular joe's"
+        styleClass="default-background"
+          
+          />
+        <Info />
+    </Layout>
+) 
+export const query = graphql`
+query {
+  img: file(relativePath: { eq: "default-background.jpg" }) {
+    childImageSharp {
+      fluid {
+        ...GatsbyImageSharpFluid_tracedSVG
+      }
+    }
+  }
+}
+`
 export default IndexPage
